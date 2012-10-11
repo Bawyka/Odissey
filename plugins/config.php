89ac085875a8f412bf99d5ae2aefe9d2a57e1b67
Bawyka <bawyka@gmail.com>
2012-10-11 14:08:29 +0300

    README UPDATED
    Was changes in README, but no code changes!

---
+++ plugins/config.php
@@ -0,0 +1,136 @@
+<?php 
+
+class Plugins { 
+
+	/* jQuery */
+	public $jquery = "<!-- jQuery -->
+<script type='text/javascript' src='plugins/jQuery/jquery-1.8.0.min.js'></script>";
+
+	/* jQuery UI */
+	public $jqueryui = "<!-- jQuery-UI: http://jqueryui.com/ -->
+<link type='text/css' href='plugins/ui/css/ui-lightness/jquery-ui-1.8.22.custom.css' rel='Stylesheet' />	
+<script type='text/javascript' src='plugins/ui/js/jquery-ui-1.8.22.custom.min.js'></script>
+<!-- // END jQuery-UI -->";	
+
+	/* jQuery jScrollPane & Mousewheel */
+	public $jscrollpane = "<!-- jScrollPane & MouseWheel http://jscrollpane.kelvinluck.com -->
+<script type='text/javascript' src='plugins/jscrollpane/jquery.mousewheel.js'></script>
+<script type='text/javascript' src='plugins/jscrollpane/jquery.em.js'></script>
+<script type='text/javascript' src='plugins/jscrollpane/mwheelIntent.js'></script>
+<script type='text/javascript' src='plugins/jscrollpane/jquery.jscrollpane.min.js'></script>
+<link   type='text/css' rel='stylesheet' href='plugins/jscrollpane/jquery.jscrollpane.css' media='all' />
+<!-- // END jScrollPane & MouseWheel -->";
+	
+	/* mtip */
+	public $mtip = "<!-- mTip-v1.0.3 http://dev.mauvm.nl/mTip/#manual -->
+<link type='text/css' rel='stylesheet' href='plugins/mTip-v1.0.3/mTip.black.css' media='all' />
+<script type='text/javascript' src='plugins/mTip-v1.0.3/mTip-v1.0.3.js'></script>
+<!-- // END mTip-v1.0.3 -->";
+
+	/* cusel */
+	public $cusel = "<!-- cusel http://www.xiper.net/collect/html-and-css-tricks/verstka-form/nice-select-jquery.html -->
+<link type='text/css' rel='stylesheet' href='plugins/cusel/css/cusel.css' media='all' />
+<script type='text/javascript' src='plugins/cusel/js/jquery-1.6.1.js'></script>
+<script type='text/javascript' src='plugins/cusel/js/cusel.js'></script>
+<script type='text/javascript' src='plugins/cusel/js/jScrollPane.js'></script>
+<script type='text/javascript' src='plugins/cusel/js/jquery.mousewheel.js'></script>
+<!-- // END cusel -->";
+
+	/* Sticky scroll menu */
+	public $stickyscrollmenu = "<!-- slidemenu http://blog.echoenduring.com/2010/11/15/freebie-contained-sticky-scroll-jquery-plugin/ -->
+<script type='text/javascript' src='plugins/stickyscrollmenu/stickyscrollmenu.js'></script>
+<!-- // END slidemenu -->";
+
+	/* jQuery Cookie */
+	public $jqcookie = "<!-- jQuery cookie: https://github.com/carhartl/jquery-cookie/ -->
+<script type='text/javascript' src='plugins/cookie/jquery.cookie.js'></script>
+<!-- // End. jQuery cookie -->";
+
+	/* jQuery scrollTo */
+	public $scrollto = "<!-- jQuery scrollTo: http://flesler.blogspot.com/2007/10/jqueryscrollto.html || gihub: https://github.com/flesler/jquery.scrollTo -->
+<script type='text/javascript' src='plugins/scrollTo/jquery.scrollTo.js'></script>
+<!-- // End. jQuery scrollTo -->";
+
+	/* jQuery checkBox (off/on) */
+	public $chkbox = "<!-- jQuery CheckBox: http://widowmaker.kiev.ua/checkbox/ -->
+<link type='text/css' rel='stylesheet' href='plugins/checkbox/jquery.checkbox.css'/>
+<link type='text/css' rel='stylesheet' href='plugins/checkbox/jquery.safari-checkbox.css'/>
+<!-- <script type='text/javascript' src='plugins/checkbox/jquery.js'></script> -->
+<script type='text/javascript' src='plugins/checkbox/jquery.checkbox.min.js'></script>
+<!-- // End. jQuery CheckBox -->";
+
+	/* jQuery Kwicks */
+	public $kwicks = "<!-- jQuery Kwicks: http://ruseller.com/adds/adds2276/example/ -->
+<link type='text/css' rel='stylesheet' href='plugins/kwicks/css/main.css' />
+<script type='text/javascript' src='plugins/kwicks/js/jquery.easing.1.3.js'></script>
+<script type='text/javascript' src='plugins/kwicks/js/jquery.kwicks.js'></script>
+<!-- // End. jQuery Kwicks -->";
+
+	/* jForm (ajaxSubmit) */
+	public $jform = "<!-- jQuery.Form.js: https://github.com/malsup/form/ -->
+<script type='text/javascript' src='plugins/jform/jquery.form.js'></script>
+<!-- // End. jQuery.Form.js -->";
+
+	/* jQuery Tables */
+	public $jtables = "<!-- jQuery Tables: http://datatables.net/ -->
+<link type='text/css' rel='stylesheet' href='plugins/DataTables-1.9.3/media/css/jquery.dataTables.css' />
+<script type='text/javascript' src='plugins/DataTables-1.9.3/media/js/jquery.dataTables.js'></script>
+<!-- // End. jQuery Tables -->";
+
+	/* jQuery Tools */
+	public $jtools = "<!-- jQuery Tools: http://jquerytools.org -->
+<script type='text/javascript' src='plugins/tools/jquery.tools.min.js'></script>
+<!-- // End. jQuery Tools -->";
+
+	/* jQuery redactor */
+	public $redactor = "<!-- jQuery redactor: http://redactorjs.com -->
+<link type='text/css' rel='stylesheet' href='plugins/redactor/redactor/redactor.css' />
+<script type='text/javascript' src='plugins/redactor/redactor/redactor.min.js'></script>
+<!-- // End. jQuery redactor -->";
+
+	/* jQuery tinymce */
+	public $tinymce = "<!-- jQuery TinyMCE: http://www.tinymce.com/ -->
+<script type='text/javascript' src='plugins/tinymce/jscripts/tiny_mce/tiny_mce.js'></script>
+<!-- // End. TinyMce -->";
+
+	/* jQuery lightbox2 */
+	public $lightbox = "<!-- jQuery Lightbox2: http://lokeshdhakar.com/projects/lightbox2/ -->
+<script type='text/javascript' src='plugins/lightbox/js/lightbox.js'></script>
+<link type='text/css' rel='stylesheet' href='plugins/lightbox/css/lightbox.css' />";
+
+	/* Multiple Files Upload */
+	public $filesupload = "<!-- jQuery FilesUpload: https://github.com/blueimp/jQuery-File-Upload/wiki/Basic-plugin -->
+<script type='text/javascript' src='plugins/filesupload/js/vendor/jquery.ui.widget.js'></script>
+<script type='text/javascript' src='plugins/filesupload/js/jquery.iframe-transport.js'></script>
+<script type='text/javascript' src='plugins/filesupload/js/jquery.fileupload.js'></script>
+<!-- // End. Files Upload  -->";
+
+	/* jQuery Image Cube */
+	public $cube = "<!-- jQuery Image Cube: http://keith-wood.name/imageCube.html -->
+<script type='text/javascript' src='plugins/cube/jquery.imagecube.min.js'></script>
+<!-- // End. jQuery Cube Image -->";
+
+	/* jQuery ajaxupload.js (Max Petrenko)*/
+	public $ajaxupload = "<!-- jQuery ajaxupload.js: max petrenko -->
+<script type='text/javascript' src='plugins/ajaxupload/ajaxupload.js'></script>
+<!-- // End. jQuery AjaxUpload -->";
+
+	/* jQuery carusel.js */
+	public $carusel = "<!-- jQuery Carusel:  http://www.thomaslanciaux.pro/jquery/jquery_carousel.htm-->
+<script type='text/javascript' src='plugins/carusel/jquery.carousel.min.js'></script>
+<!-- // End. jQuery Carusel -->";
+
+	/* jQuery Highlight */
+	public $highlight = "<!-- jQuery Highlight: http://softwaremaniacs.org/soft/highlight/en/download/ -->
+<link rel='stylesheet' href='plugins/highlight/styles/default.css'>
+<script type='text/javascript' src='plugins/highlight/highlight.pack.js'></script>
+<script>hljs.initHighlightingOnLoad();</script>
+<!-- // End. jQuery Highlight -->";
+
+	/* jQuery color picker */
+	public $colorpicker = "<!-- jQuery ColorPicker: http://www.eyecon.ro/colorpicker/#about -->
+<link rel='stylesheet' media='screen' type='text/css' href='plugins/colorpicker/css/colorpicker.css' />
+<script type='text/javascript' src='plugins/colorpicker/js/colorpicker.js'></script>
+<!-- // End. jQuery colorpicker -->";
+
+}
